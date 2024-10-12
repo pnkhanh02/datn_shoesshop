62955b3a38bcf0bd95b2f8c08e2ada3b9b7ba515
pnkhanh02 <115911423+pnkhanh02@users.noreply.github.com>
2024-10-12 11:13:27 +0700

    install the system web

---
+++ shoesshop_backend/shoesshop/src/main/java/com/example/shoesshop/ShoesshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesshopApplication.class, args);
+	}
+
+}
---
+++ shoesshop_backend/shoesshop/src/test/java/com/example/shoesshop/ShoesshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoesshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
